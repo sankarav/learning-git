72e1fdcee155950a439089eba1cde51f2dc0c2d6
Sankara Vadivel Dhandapani <san.gct@gmail.com>
2016-07-06 13:14:28 -0500

    Changed Something to Student.java

--- src/main/java/edu/ub/san/Something.java
+++
@@ -1,7 +0,0 @@
-package edu.ub.san;
-
-/**
- * Created by dhandapa on 7/2/16.
- */
-public class Something {
-}
---
+++ src/main/java/edu/ub/san/Student.java
@@ -0,0 +1,15 @@
+package edu.ub.san;
+
+/**
+ * Created by dhandapa on 7/2/16.
+ */
+public class Student {
+	int age;
+	public Student() {
+
+	}
+
+	public Student(int age) {
+		this.age = age;
+	}
+}
