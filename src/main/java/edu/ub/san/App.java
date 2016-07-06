dbcee9ee9b82bb3a42e0124352515fa9b0b25d46
Sankara Vadivel Dhandapani <san.gct@gmail.com>
2016-07-06 18:16:53 -0500

    Modified App.java

--- src/main/java/edu/ub/san/App.java
+++ src/main/java/edu/ub/san/App.java
@@ -2,9 +2,11 @@ package edu.ub.san;
 
 /**
  * Hello world!
+ * More changes
  */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("More changes goes here");
 	}
 }
