75f5650f614a8a43f50c0e8fa80f0acd3d90c79e
Sankara Vadivel Dhandapani <san.gct@gmail.com>
2016-07-06 19:05:27 -0500

    Stashed App.java applied and pushed

--- src/main/java/edu/ub/san/App.java
+++ src/main/java/edu/ub/san/App.java
@@ -8,5 +8,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("More changes goes here");
+		System.out.println("Gonna stash this");
 	}
 }
