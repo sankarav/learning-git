8cd3c3e701a5c2ad4064c724e1044b996396369f
Sankara Vadivel Dhandapani <san.gct@gmail.com>
2016-07-09 00:11:19 -0500

    Change App.java in development branch

--- src/main/java/edu/ub/san/App.java
+++ src/main/java/edu/ub/san/App.java
@@ -9,5 +9,6 @@ public class App {
 		System.out.println("Hello World!");
 		System.out.println("More changes goes here");
 		System.out.println("Gonna stash this");
+		System.out.println("Changed App.java in development branch");
 	}
 }
