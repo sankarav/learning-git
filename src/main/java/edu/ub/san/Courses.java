f1d3cb08916704ed0b34507a1c4d812f69d9e530
Sankara Vadivel Dhandapani <san.gct@gmail.com>
2016-07-25 21:22:06 -0500

    Added Courses.java in master branch

---
+++ src/main/java/edu/ub/san/Courses.java
@@ -0,0 +1,18 @@
+package edu.ub.san;
+
+/**
+ * Created by dhandapa on 7/25/16.
+ */
+public class Courses {
+	private int courseId;
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+
+}
