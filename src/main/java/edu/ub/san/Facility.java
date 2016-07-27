62b89c281845704bbe32b1f346ae91dca66b992f
Sankara Vadivel Dhandapani <san.gct@gmail.com>
2016-07-26 22:17:27 -0500

    Ammend commit of Added Location (lat, lon) to Facility

--- src/main/java/edu/ub/san/Facility.java
+++ src/main/java/edu/ub/san/Facility.java
@@ -6,14 +6,33 @@ package edu.ub.san;
 public class Facility {
 	private int id;
 	private String name;
+	private double lat, lon;
 
 	@Override
 	public String toString() {
 		final StringBuilder sb = new StringBuilder("Facility{");
 		sb.append("id=").append(id);
 		sb.append(", name='").append(name).append('\'');
+		sb.append(", lat=").append(lat);
+		sb.append(", lon=").append(lon);
 		sb.append('}');
 		return sb.toString();
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
 	}
 
 	public int getId() {
